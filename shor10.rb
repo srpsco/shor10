b91f323557b363b190d69861f9341eb990a87102
Steve Robillard <steverobillard@gmail.com>
2011-07-11 00:18:04 -0400

    first working copy

--- shor10.rb
+++ shor10.rb
@@ -7,7 +7,7 @@ DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/developm
 class URL
   include DataMapper::Resource  
   property :id,           Serial
-  property :url,          String
+  property :url,          Text
 end
 
 DataMapper.auto_upgrade!
@@ -20,6 +20,20 @@ end
 
 # Display original URL
 get '/:id' do
-  @url = URL.get(params[:id])
-  erb :url
+  url = URL.get(params[:id])
+  redirect url.url
 end
+
+# Shorten the URL
+post '/url/shorten' do
+  url = URL.new(:url => params[:url])
+  if url.save
+#    status 201
+#    redirect '/'+url.id.to_s 
+     @shorturl = url
+     erb :shortened
+   else
+    status 412
+    redirect '/'   
+  end
+end 
